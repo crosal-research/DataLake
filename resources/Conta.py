4159e119b8ecacd3f89746b117b66df1b5a770b8
jmrosal <jmrosal11@gmail.com>
2023-05-31 06:56:52 -0300

    adds async to Conta.py file

--- resources/Conta.py
+++ resources/Conta.py
@@ -1,5 +1,5 @@
 # import from system
-import json, io
+import json, io, asyncio
 
 # import from packcages
 import falcon
@@ -15,7 +15,22 @@ class Conta:
     """
     async def on_get(self, req, resp):
         """Handles GET requests"""
-        pass
+        q = req.get_param_as_list('account', required=False)
+        tp = req.get_param('type', required=False)
+        resp.status = falcon.HTTP_200
+        loop = asyncio.get_running_loop()
+        df = await loop.run_in_executor(None, conta.query_conta, q)
+        output = io.StringIO()
+
+        if tp == 'json':
+            jdf = [{'conta_id': df.loc[s, 'conta_id'], 
+                    'nome': df.loc[s, 'nome']  } for s in df.index]
+            resp.text = json.dumps(jdf)
+                
+        else:
+            df.to_csv(output)
+            resp.text = output.getvalue()
+
 
     async def on_post(self, req, resp):
         pass
