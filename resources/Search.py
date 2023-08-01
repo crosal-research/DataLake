5add8696e5618b61fcda923d8e456482fc2974fb
João Mauricio Rosal <jmrosal11@gmail.com>
2023-08-01 16:20:19 -0300

    fix key name of the json returned to descricao from descripcao

--- resources/Search.py
+++ resources/Search.py
@@ -13,18 +13,21 @@ class Search:
     a pesquisa de series na base de dados
     """
     async def on_get(self, req, resp):
-        """Handles GET requests"""
+        """
+        retrieves series related to a search string
+        issued by user. Word is the string and type 
+        is the return's format: either csv or json
+        """
         q = req.get_param('words', required=False)
         tp = req.get_param('type', required=False)
         resp.status = falcon.HTTP_200
-        if q:           
+        if q:
             df = search.query_search(q)
-            print(df.head())
             output = io.StringIO()
             if tp == 'json':
                 resp.text = json.dumps([{'rank': 1, 
                                          'ticker': df.loc[i, 'tickers'],
-                                         'descripcao': df.loc[i, 'descricao']} 
+                                         'descricao': df.loc[i, 'descricao']} 
                                         for i in df.index])
             else:
                 df.to_csv(output)
